72152c4452f89fe1c6fdd59aaf782e4b80b6ad85
jaspinall <julia.stefanczyk@gmail.com>
2016-11-01 19:16:08 -0700

    more fun with react

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.searchProjects = this.searchProjects.bind(this);
+    this.addProject = this.addProject.bind(this);
   }
 
   handleChange(event) {
