cb3fdac9af566a8561d93c92eb48a87f5070d031
Anthony Sarno <asarno17@gmail.com>
2016-11-01 17:17:26 -0700

    updated

--- src/client/components/Dashboard.js
+++ src/client/components/Dashboard.js
@@ -14,9 +14,18 @@ class Dashboard extends Component {
 
     return (
       <div className="dashboard" style={{display: (this.props.showDashboard) ? 'block' : 'none'}}>
+        <div className="welcome">
+           <img src={this.props.userInfo.url}></img>
+        <ul>
+          <li>Username: {this.props.userInfo.username}</li>
+          <li>Name: {this.props.userInfo.name}</li>
+          <li>Company: {this.props.userInfo.company}</li>
+          <li>Bio: {this.props.userInfo.bio}</li>
+        </ul>
+      </div>
         <div>
-          <input value={this.props.searchInput} type="text"/>
-          <button type="submit">Show me projects!</button>
+          <input value={this.props.searchValue} onChange={this.props.handleChange} type="text"/>
+          <button type="submit" onClick={this.props.handleSubmit}>Show me projects!</button>
           <br/>
           <SearchResults searchResults={this.props.searchResults}/>
         </div>
